e3cf541243b047c8c5d27feaa4240ffba7ffacc2
Abhi Singh <abhisingh@Abhis-MBP.wifi.gsu.edu>
2022-03-31 10:36:26 -0400

    set port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+import os
 
 app = flask.Flask(__name__)
 
@@ -6,3 +7,9 @@ app = flask.Flask(__name__)
 @app.route("/")
 def login():
     return flask.render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True
+    )
